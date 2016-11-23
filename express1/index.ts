301ed408f2f25c0fca7e67217069dba22da3b64f
ovrmrw <t.ngc.0403@gmail.com>
2016-11-23 16:01:00 +0900

    Fix

--- express1/index.ts
+++ express1/index.ts
@@ -6,9 +6,10 @@ import * as express from 'express';
 
 // Create express app as usual 
 const app = express();
-app.get('/api/:foo/:bar', (req, res) => {
+app.get('/foo/:foo/:bar', (req, res) => {
   res.json({
-    result: '[result]'
+    foo: req.params.foo,
+    bar: req.params.bar
   });
 });
 
