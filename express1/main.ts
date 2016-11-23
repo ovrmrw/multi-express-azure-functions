e59d7a9360b9075aaea894d92e2f1fb5714d65e0
ovrmrw <t.ngc.0403@gmail.com>
2016-11-23 17:03:42 +0900

    Fix

--- express1/main.ts
+++ express1/main.ts
@@ -1,11 +1,15 @@
 import * as express from 'express';
+import * as http from 'http';
 
 const app = express();
-app.get('/express1/foo', (req, res) => res.json({ 'test': 'ok' }));
+app.get('/foo', (req, res) => res.json({ 'test': 'ok' }));
 app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
 app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 // app.get('/express1/foo',(req,res)=>)
-app.listen(process.env.PORT);
+
+const server = http.createServer(app);
+server.listen('/express1');
+
 
 import { AzureFunction } from '../types';
 import { passedTimeMessage, logResponse } from '../lib/utils';
@@ -18,6 +22,10 @@ export const azureFunction: AzureFunction =
       status: 200,
       body: 'test is ok'
     };
+
+    server.on('listening',()=> {
+      context.log('listing');
+    });
 
     context.log('req.params.route', req.params.route);
     context.log(context);
