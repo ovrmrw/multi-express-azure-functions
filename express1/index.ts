3a3f0b41293f51db11acf0397766361a167c2140
ovrmrw <t.ngc.0403@gmail.com>
2016-11-23 16:27:59 +0900

    Fix

--- express1/index.ts
+++ express1/index.ts
@@ -1,23 +1,7 @@
-// const createAzureFunctionHandler = require('azure-function-express').createAzureFunctionHandler;
+import { azureFunction } from './main';
+import { auth0AuthenticationHook } from '../lib/auth';
+import { wrapper } from '../lib/utils';
 
-import * as express from 'express';
 
-import { AFContext, AFRequest } from '../types';
-
-// const express = require("express");
-
-// Create express app as usual 
-const app = express();
-app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
-app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
-
-// Binds the express app to an Azure Function handler 
-// export default createAzureFunctionHandler(app);
-
-export default async (context: AFContext, req: AFRequest) => {
-  context.res = {
-    status: 200,
-    body: context
-  }
-  context.done();
-}
+// export default auth0AuthenticationHook(lodashAzureFunction);
+export default azureFunction;
---
+++ express1/main.ts
@@ -0,0 +1,23 @@
+import * as express from 'express';
+
+const app = express();
+app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
+app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
+
+
+import { AzureFunction } from '../types';
+import { passedTimeMessage, logResponse } from '../lib/utils';
+
+
+export const azureFunction: AzureFunction =
+  async (context, req) => {
+    const startTime = new Date().getTime();
+    context.res = {
+      status: 200,
+      body: 'test is ok'
+    };
+    
+    context.log(...logResponse(context));
+    context.log(passedTimeMessage(startTime));
+    context.done();
+  };
