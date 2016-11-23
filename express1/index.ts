c6c1196e4f24b3a9888d160f6d2ef344b7ba2694
ovrmrw <t.ngc.0403@gmail.com>
2016-11-23 16:17:51 +0900

    Fix

--- express1/index.ts
+++ express1/index.ts
@@ -2,6 +2,8 @@ const createAzureFunctionHandler = require('azure-function-express').createAzure
 
 import * as express from 'express';
 
+import { AFContext, AFRequest } from '../types';
+
 // const express = require("express");
 
 // Create express app as usual 
@@ -10,4 +12,12 @@ app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
 app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler 
-export default createAzureFunctionHandler(app);
+// export default createAzureFunctionHandler(app);
+
+export default async (context: AFContext, req: AFRequest) => {
+  context.res = {
+    status: 200,
+    body: context
+  }
+  context.done();
+}
