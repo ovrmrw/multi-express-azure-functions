8fdca409ad4b98163a69d3128d28be6a99668381
ovrmrw <t.ngc.0403@gmail.com>
2016-11-25 22:29:19 +0900

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,66 +7,67 @@ const webpack = require('webpack');
 
 
 const loaders = [
-  {
-    test: /\.ts$/,
-    loader: "ts-loader",
-    exclude: /node_modules/,
-  },
-  {
-    test: /\.json$/,
-    loader: "json-loader",
-    exclude: /\.\/secret(-|)key/i,
-  },
-  {
-    test: /\.js$/,
-    loader: 'babel-loader',
-    query: {
-      presets: ['latest'],
-      plugins: []
+    {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
     },
-    // exclude: /(node_modules|bower_components)/,
-  }
+    {
+        test: /\.json$/,
+        loader: "json-loader",
+        exclude: /\.\/secret(-|)key/i,
+    },
+    {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['latest'],
+            plugins: []
+        },
+        // exclude: /(node_modules|bower_components)/,
+    }
 ];
 
 
 module.exports = [
-  {
-    name: 'server bundle',
-    target: 'node',
-    entry: {
-      'hapi1': './hapi1/main.ts',
-      'express1': './express1/main.ts',
-      'lodash': './lodash/main.ts',
-    },
-    output: {
-      filename: DEST + '/[name]/main.js',
-      libraryTarget: "commonjs2"
-    },
-    resolve: {
-      extensions: ['.js', '.ts']
-    },
-    externals: [
-      {
-        '../../secret-key/app.secret.json': '../secret-key/app.secret.json',
-        '../../secret-key/serviceAccountKey.json': '../secret-key/serviceAccountKey.json',
-        'firebase': 'firebase',
-        'firebase-admin': 'firebase-admin',
-      }
-    ],
-    plugins: [
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    ],
-    // plugins: [
-    //   new webpack.optimize.UglifyJsPlugin({
-    //     mangle: true,
-    //     compress: {
-    //       warnings: false
-    //     }
-    //   }),
-    // ],
-    module: {
-      loaders: loaders
+    {
+        name: 'server bundle',
+        target: 'node',
+        entry: {
+            'hapi1': './hapi1/main.ts',
+            'express1': './express1/main.ts',
+            'lodash': './lodash/main.ts',
+        },
+        output: {
+            filename: DEST + '/[name]/main.js',
+            libraryTarget: "commonjs2"
+        },
+        resolve: {
+            extensions: ['.js', '.ts']
+        },
+        externals: [
+            {
+                '../../secret-key/app.secret.json': '../secret-key/app.secret.json',
+                '../../secret-key/serviceAccountKey.json': '../secret-key/serviceAccountKey.json',
+                'firebase': 'firebase',
+                'firebase-admin': 'firebase-admin',
+            }
+        ],
+        plugins: [
+            // Ignore all locale files of moment.js
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        ],
+        // plugins: [
+        //   new webpack.optimize.UglifyJsPlugin({
+        //     mangle: true,
+        //     compress: {
+        //       warnings: false
+        //     }
+        //   }),
+        // ],
+        module: {
+            loaders: loaders
+        }
+        // devtool: 'source-map',
     }
-    // devtool: 'source-map',
-  }
 ];
